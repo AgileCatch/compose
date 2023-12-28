4f93492991e822971aaf7673ee0fced21fd968d9
yeonghyeon <agilecatch@gmail.com>
2023-12-28 16:49:47 +0900

    [Feat] : Box안 contentAlignment로 글씨정렬

--- Box/app/src/main/java/com/example/box/MainActivity.kt
+++ Box/app/src/main/java/com/example/box/MainActivity.kt
@@ -3,22 +3,32 @@ package com.example.box
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.box.ui.theme.BoxTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Box{
+        setContent {//안에있는 내용들이 겹칠 수 있는 성질을가짐
+            Box(
+                modifier = Modifier
+                    .background(Color.Gray)
+                    .fillMaxSize(),
+                contentAlignment = Alignment.CenterEnd,//글씨정렬 속성
+            ) {
                 Text(text = "Hello")
+                Text(text = "12345~~~~")
             }
         }
     }
